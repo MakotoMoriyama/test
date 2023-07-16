977859c1a38bd5257d57d689d6ab5723cd85e177
MakotoMoriyama <139620846+MakotoMoriyama@users.noreply.github.com>
2023-07-17 08:38:45 +0900

    Delete accounts directory

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Users
-from django.contrib.auth.models import Group
-
-admin.site.unregister(Group)  
-
-@admin.register(Users)
-class UsersAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user_name', 'email', 'address', 'regular_holiday',)
